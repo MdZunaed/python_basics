ee64f11567deaa716b4eaaa4b7f2ab5f1524b505
Zunayed Ebney Faruk <122368527+MdZunaed@users.noreply.github.com>
2024-05-02 00:50:38 +0600

    render_template

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+# @app.route('/<user>')
+# def success(user):
+#     return render_template('index.html', name = user)
+# # app.add_url_rule('/', 'hello', helloWorld)
+
 @app.route('/')
-def helloWorld():
-    return "Welcome to Flask"
-# app.add_url_rule('/', 'hello', helloWorld)
+def index():
+        return render_template('index.html')
+
+@app.route('/booking')
+def booking():
+        return render_template('booking.html')
 
 if __name__== "__main__":
-        app.run()
+        app.run(debug= True)
 
