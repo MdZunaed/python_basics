18181ba31574e89fafa19fda92ccd7cc00e0cabe
Zunayed Ebney Faruk <122368527+MdZunaed@users.noreply.github.com>
2024-05-02 23:35:55 +0600

    loop

--- data_types.py
+++ data_types.py
@@ -12,42 +12,45 @@ dictionaryType = {'name': "Zunayed", 'roll': 450197, 'male': True}
 ########## List ##########
 
 # list is mutable(changable), ordered, []
-# listType.append('apple')
-# listType.insert(1,'orange')
-# listType.remove('mango')
-# listType.reverse()
-# print(listType.count)
+
+listType.append('apple')
+listType.insert(1,'orange')
+listType.remove('mango')
+listType.reverse()
+print(listType.count)
 
 ########## Tuple ##########
 
 # tuple is immutable(not changable), ordered, able to duplicate ()
-# listFromTuple= list(tupleType)
-# listFromTuple.append('johan')
-# tupleType = listFromTuple
-# print(tupleType)
+
+listFromTuple= list(tupleType)
+listFromTuple.append('johan')
+tupleType = listFromTuple
+print(tupleType)
 
 ########## Set ##########
 
 # set is mutable(changable), unordered, doesn't allow duplicate {}
-# setTwo = {2,4,6,12}
-# setType.add(50)
-# setType.remove(8)
-# setTwo.update(setType)
-# print(setType)
-# print(setTwo)
+
+setTwo = {2,4,6,12}
+setType.add(50)
+setType.remove(8)
+setTwo.update(setType)
+print(setType)
+print(setTwo)
 
 ########## Dictionary ##########
 
 # dictionary is key value pair, mutable(changable), doesn't allow duplicate 'key'{'key': 'value'}
 
-# print(dictionaryType)
-# print(dictionaryType['name'])
-# dictionaryType['roll'] = 1
-# print(dictionaryType)
-# dictionaryType['result'] = 4.85
-# print(dictionaryType)
-# dictionaryType.pop('male')
-# print(dictionaryType)
+print(dictionaryType)
+print(dictionaryType['name'])
+dictionaryType['roll'] = 1
+print(dictionaryType)
+dictionaryType['result'] = 4.85
+print(dictionaryType)
+dictionaryType.pop('male')
+print(dictionaryType)
 
 
 
---
+++ loop.py
@@ -0,0 +1,58 @@
+
+######### For loop #########
+
+##for i in range(start,end,skip):
+
+for i in range(1,11,2):
+    print(i)
+
+###### 1+2+3+4+5+....+100 = ?
+
+sum = 0
+for i in range(1,101):
+    sum = sum+i #shortcut-> sum += 1
+    print(sum)
+
+###### 1+3+5+7+...+99 = ? 
+## method 1:
+
+sum = 0
+for i in range(1,100, 2):
+    sum = sum+ i
+    print(sum)
+
+## method 2:
+
+sum = 0
+for i in range(1,101):
+    if i % 2 == 0:
+        continue
+    else:
+        sum = sum + i
+        print(sum)    
+
+## method 3:
+
+sum = 0
+for i in range(1,101):
+    if i % 2 != 0:
+        sum = sum + i
+        print(sum)
+
+fruits = ["banana", "mango", "Apple"]
+
+for item in fruits:
+    print(item)    
+
+######### While loop #########
+
+## while condition:
+#         statement
+         
+###### 1+3+5+7+...+99 = ? 
+
+# sum = 0
+# while 1 < 101:
+#     sum = sum+1
+# print(sum)
+
