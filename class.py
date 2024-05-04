72a5a306617321793df3eda2f84b946f393a006a
Zunayed Ebney Faruk <122368527+MdZunaed@users.noreply.github.com>
2024-05-04 09:57:25 +0600

    class

---
+++ class.py
@@ -0,0 +1,10 @@
+class human:
+    name = ''
+    height = 0.0
+
+zunaed = human
+zunaed.height= 5.8
+zunaed.name = "Zunayed Ebney Faruk"
+
+print(f"zunaed's name is {zunaed.name}")
+print(f"zunaed's height is{zunaed.height} feet")
