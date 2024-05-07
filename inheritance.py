65895955400bdd87157d566d8485191d6735cb0e
zunaedL <mdzunaed4@gmail.com>
2024-05-07 23:40:14 +0600

    multiple & multilevel inheritance

--- inheritance.py
+++ inheritance.py
@@ -3,12 +3,36 @@ class ParentClass:
     land = "100 sq/feet"
     business = "dollar business"
 
+class ParentClassTwo:
+    car = "BMW"
+    bike = "Suzuki"
+
 
 class ChildClass(ParentClass):
     job = "private job"
 
-parent = ParentClass
-child = ChildClass
+parent = ParentClass()
+child = ChildClass()
 
 print(child.land)
 print(child.house)
+
+
+########## Multiple Inheritance #################
+
+class ChildClassTwo(ParentClass, ParentClassTwo):
+    nothing = "Nothing"
+    
+childTwo = ChildClassTwo()
+print(childTwo.land)
+print(childTwo.bike)
+
+
+########## Multi-level Inheritance #################
+
+class ChildClassThree(ChildClass):
+    nothing = "Nothing"
+    
+childThree = ChildClassThree()
+print(childThree.job)
+print(childThree.business)
